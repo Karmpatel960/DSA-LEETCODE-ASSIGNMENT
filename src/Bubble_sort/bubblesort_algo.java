dfd08c152c8491d72d66e2dbc10e69af50379581
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-06-09 20:36:08 +0530

    merge sorted array solved

--- src/Bubble_sort/bubblesort_algo.java
+++ src/Bubble_sort/bubblesort_algo.java
@@ -1,9 +1,33 @@
 package Bubble_sort;
 
+import java.util.Arrays;
+
 public class bubblesort_algo {
     public static void main(String[] args) {
-        int[] arr = new int[10];
-        
+        int[] arr = {10, 60, 40, 50, 30, 20};
+        bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
+
+    public static void bubblesort(int[] arr) {
+        boolean swap;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            swap = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if(arr[j]<arr[j-1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = t;
+                    swap=true;
+                    count++;
+                }
+            }
+            if(!swap){
+                break;
+            }
+        }
+        System.out.println(count);
+    }
 }
