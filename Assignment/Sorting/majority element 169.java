3743793fc67d447180a519a224125e309cf37c0f
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-06-10 23:45:06 +0530

    majority elemet question solved

---
+++ Assignment/Sorting/majority element 169.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                majority = nums[i];
+                count = 1;
+            } else if (nums[i] == majority) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return majority;
+    }
+}
