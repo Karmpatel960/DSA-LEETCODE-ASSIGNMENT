b8ef412aebf9573c2435292a6ce1f152f53c4a6b
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-06-13 23:27:35 +0530

    valid anagram added

---
+++ Assignment/Array/matchanagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] c = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            c[s.charAt(i) - 'a']++;
+            c[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] != 0) {
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+}
