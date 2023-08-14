25e8f35e37c39724d8cb17c664712e750889e64f
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-08-15 01:20:33 +0530

    gcd and lsm added

---
+++ src/Math2/GCD.java
@@ -0,0 +1,20 @@
+package Math2;
+
+public class GCD {
+    public static void main(String[] args) {
+        System.out.println(gcd(4,9));
+
+        System.out.println(gcd(9,18));
+    }
+
+    static int gcd(int a,int b){
+        if(a == 0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+
+    static int lcm(int a,int b){
+        return a * b /gcd(a,b);
+    }
+}
