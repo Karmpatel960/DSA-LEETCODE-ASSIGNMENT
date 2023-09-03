52dbb291ffcf77f7e7447cf8b3cdd6736a3f7875
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-09-03 22:24:35 +0530

    array added

---
+++ ATOZSHEET/INTRODSA/array_string.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int arr[5];
+    // cin >> arr[1] >> arr[2] >> arr[3]>> arr[4] >> arr[5];
+
+    // arr[3] = 16;
+    // cout <<arr[3];
+
+
+    //2d array with 3 rows and 4 columns
+    int arr2d[3][4];
+    //
+    arr2d[1][3] = 78;
+    cout << arr2d[1][3];
+
+
+    string s = "Striver";
+    int len = s.length();
+    s[len-1] = 's';
+    cout << s;
+    return 0;
+}
