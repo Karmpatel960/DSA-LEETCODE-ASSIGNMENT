17f9f50327e610142190980ac31ae6bd8c94f73c
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-09-20 00:27:03 +0530

    selection sort added

---
+++ ATOZSHEET/sort/selectionsort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void selectionsort(int arr[], int n){
+    for (int  i = 0; i < n -2; i++)
+    {
+
+        int min = i;
+        for (int j = i; j < n-1; j++)
+        {
+            /* code */  
+            if(arr[j]< arr[min]){
+                min = j;
+            }
+        }
+
+         int temp = arr[min];
+         arr[min] = arr[i];
+        arr[i] = temp;
+        
+        /* code */
+    }
+    
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,7,6,8,9,10};
+    int n = 10;
+    selectionsort(arr,n);
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout<< arr[i] << endl;
+    }
+
+    return 0;
+    
+}
+
